aa0f800e4b2b3b4f0b6bbdbbfe20ebe9aa8e9def
czb44 <clarkburleson4@gmail.com>
2026-08-12 23:14:47 -0400

    feat: table of contents

--- src/lecflow/notes.py
+++ src/lecflow/notes.py
@@ -4,11 +4,17 @@ from .unit_type.rules import classify_unit_type
 def generate_notes(blocks: dict[int, list[str]], topic_labels: dict[int, str], housekeeping_sentences: list[str]) -> str:
     '''Converts cleaned transcript to a Markdown-formatted string'''
     heading = '# Lecture Notes\n\n'
+    toc_header = '## Table of Contents\n\n'
+
+    toc = ''
     body = ''
 
     #Display sentences of each block in natural lecture flow
     for i, block in blocks.items():
-        body += f'## {topic_labels[i].title()}\n\n'
+        label = topic_labels[i].title()
+        link_case = label.strip().lower().replace("'", "").replace(" ","-")
+        body += f'## {label}\n\n'
+        toc += f'- [{label}](#{link_case})\n'
         for sentence in block:
             unit_type = classify_unit_type(sentence)
             if unit_type == 'example':
@@ -20,13 +26,15 @@ def generate_notes(blocks: dict[int, list[str]], topic_labels: dict[int, str], h
             elif unit_type == 'explanation': #no label for content
                 body += f'- {sentence}\n'
         body += '\n\n'
-    
+
+    toc += f'- [Housekeeping](#housekeeping)\n\n\n'
+
     housekeeping_head = '## Housekeeping\n\n'
     housekeeping_body = ''
     for sent in housekeeping_sentences:
         housekeeping_body += f'- {sent}\n'
 
-    return heading + body + housekeeping_head + housekeeping_body
+    return heading + toc_header + toc + body + housekeeping_head + housekeeping_body
 
 def save_notes(notes: str, output_path: Path) -> None:
     '''Ensures output folder exists and writes notes (Markdown string)
